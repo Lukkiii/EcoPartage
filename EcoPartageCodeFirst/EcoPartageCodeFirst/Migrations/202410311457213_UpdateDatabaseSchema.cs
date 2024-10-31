33aeab80ffcf98ab7a428e5e87db9befa2c50016
Lukkiii <luyuxuan2000a@gmail.com>
2024-10-31 19:47:29 +0100

    pas encore finis

---
+++ EcoPartageCodeFirst/EcoPartageCodeFirst/Migrations/202410311457213_UpdateDatabaseSchema.cs
@@ -0,0 +1,118 @@
+﻿namespace EcoPartageCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDatabaseSchema : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
+            DropIndex("dbo.Posts", new[] { "BlogId" });
+
+            CreateTable(
+            "dbo.Users",
+            c => new
+            {
+                idUser = c.Int(nullable: false, identity: true),
+                UserName = c.String(),
+                Email = c.String(),
+                Password = c.String(),
+                Role = c.String(),
+            })
+            .PrimaryKey(t => t.idUser);
+
+            CreateTable(
+                "dbo.Annonces",
+                c => new
+                    {
+                        idAnnonce = c.Int(nullable: false, identity: true),
+                        Titre = c.String(),
+                        Description = c.String(),
+                        Points = c.Int(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                        Active = c.Boolean(nullable: false),
+                        idUser = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.idAnnonce)
+                .ForeignKey("dbo.Users", t => t.idUser, cascadeDelete: true)
+                .Index(t => t.idUser);
+            
+
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        idComment = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        Notice = c.String(),
+                        idUserGiven = c.Int(),
+                        idUserRecipient = c.Int(),
+                        Users_idUser = c.Int(),
+                        Users_idUser1 = c.Int(),
+                    })
+                .PrimaryKey(t => t.idComment)
+                .ForeignKey("dbo.Users", t => t.idUserGiven)
+                .ForeignKey("dbo.Users", t => t.idUserRecipient)
+                .ForeignKey("dbo.Users", t => t.Users_idUser)
+                .ForeignKey("dbo.Users", t => t.Users_idUser1)
+                .Index(t => t.idUserGiven)
+                .Index(t => t.idUserRecipient)
+                .Index(t => t.Users_idUser)
+                .Index(t => t.Users_idUser1);
+            
+            DropTable("dbo.Blogs");
+            DropTable("dbo.Posts");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Username = c.String(nullable: false, maxLength: 128),
+                        DisplayName = c.String(),
+                    })
+                .PrimaryKey(t => t.Username);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        PostId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Content = c.String(),
+                        BlogId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PostId);
+            
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        BlogId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Url = c.String(),
+                    })
+                .PrimaryKey(t => t.BlogId);
+            
+            DropForeignKey("dbo.Annonces", "idUser", "dbo.Users");
+            DropForeignKey("dbo.Comments", "Users_idUser1", "dbo.Users");
+            DropForeignKey("dbo.Comments", "Users_idUser", "dbo.Users");
+            DropForeignKey("dbo.Comments", "idUserRecipient", "dbo.Users");
+            DropForeignKey("dbo.Comments", "idUserGiven", "dbo.Users");
+            DropIndex("dbo.Comments", new[] { "Users_idUser1" });
+            DropIndex("dbo.Comments", new[] { "Users_idUser" });
+            DropIndex("dbo.Comments", new[] { "idUserRecipient" });
+            DropIndex("dbo.Comments", new[] { "idUserGiven" });
+            DropIndex("dbo.Annonces", new[] { "idUser" });
+            DropTable("dbo.Comments");
+            DropTable("dbo.Users");
+            DropTable("dbo.Annonces");
+            CreateIndex("dbo.Posts", "BlogId");
+            AddForeignKey("dbo.Posts", "BlogId", "dbo.Blogs", "BlogId", cascadeDelete: true);
+        }
+    }
+}
--- EcoPartageCodeFirst/EcoPartageCodeFirst/Program.cs
+++ EcoPartageCodeFirst/EcoPartageCodeFirst/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace EcoPartageCodeFirst
@@ -15,51 +16,138 @@ namespace EcoPartageCodeFirst
         {
             using (var db = new BloggingContext())
             {
-                // Saisit, Crée et Sauvegarde un nouveau Blog.
-                Console.Write("Entrez un nom pour votre nouveau Blog : ");
+                // Saisit, Crée et Sauvegarde un nouveau User.
+                Console.Write("Entrez un Username pour enregistrement : ");
                 var name = Console.ReadLine();
-                var blog = new Blog { Name = name };
-                db.Blogs.Add(blog);
+                Console.Write("Entrez un Email pour enregistrement : ");
+                var email = Console.ReadLine();
+                Console.Write("Entrez un Password pour enregistrement : ");
+                var password = Console.ReadLine();
+                var user = new Users { UserName = name, Email = email, Password = password , Role = null}; 
+                db.Users.Add(user);
                 db.SaveChanges();
                 // Sélectionne et affiche tous les blogs depuis la BD.
-                var query = from b in db.Blogs
-                            orderby b.Name
-                            select b;
-                Console.WriteLine("Tous les blogs dans la BD :");
+                var query = from u in db.Users
+                            orderby u.UserName
+                            select u;
+                Console.WriteLine("Tous les Usrs dans la BD :");
                 foreach (var item in query)
                 {
-                    Console.WriteLine(item.Name);
+                    Console.WriteLine(item.UserName);
                 }
                 Console.WriteLine("Pressez une touche pour quitter...");
                 Console.ReadKey();
             }
         }
-        public class Blog
+
+        public class Users
         {
-            public int BlogId { get; set; }
-            public string Name { get; set; }
-            public string Url { get; set; }
-            public virtual List<Post> Posts { get; set; }
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Key]
+            public int idUser { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string Role { get; set; }
+            public ICollection<Comments> CommentsGiven { get; set; }
+            public ICollection<Comments> CommentsRecived { get; set; }
         }
-        public class Post
-        {
-            public int PostId { get; set; }
-            public string Title { get; set; }
-            public string Content { get; set; }
-            public int BlogId { get; set; }
-            public virtual Blog Blog { get; set; }
-        }
-        public class User
+        public class Annonces
         {
             [Key]
-            public string Username { get; set; }
-            public string DisplayName { get; set; }
+            public int idAnnonce { get; set; }
+            public string Titre { get; set; }
+            public string Description { get; set; }
+            public int Points { get; set; }
+            public DateTime Date { get; set; }
+            public bool Active { get; set; }
+
+            [ForeignKey("Users")]
+            public int idUser { get; set; }
+            public Users Users { get; set; }
         }
+
+        public class Comments
+        {
+            [Key]
+            public int idComment { get; set; }
+            public DateTime Date { get; set; }
+            public string Notice { get; set; }
+            [ForeignKey("Giver")]
+            public int? idUserGiven { get; set; }
+            [ForeignKey("Recipient")]
+            public int? idUserRecipient { get; set; }
+            public Users Giver { get; set; }
+            public Users Recipient { get; set; }
+        }
+
+        public class Transactions
+        {
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Key]
+            public int IdTransaction { get; set; }
+            [Required]
+            [ForeignKey("IdUser")] // §!!!§ vérifier nom de la clé étrangère
+            public int UserIdGiver { get; set; }
+            [Required]
+            [ForeignKey("IdUser")] // §!!!§ vérifier nom de la clé étrangère
+            public int UserIdRecipient { get; set; }
+            [Required]
+            [ForeignKey("IdAnnonce")]
+            public int IdAnnonce { get; set; }
+            [ForeignKey("Points")] // Table Annonces
+            public int AnnoncePoints { get; set; }
+            [Required]
+            public DateTime DateTransaction { get; set; }
+
+        }
+        public class Tags
+        {
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Key]
+            public int IdTag { get; set; }
+            [Required]
+            public string CategoryName { get; set; }
+        }
+        public class AnnoncesTags
+        {
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Key]
+            public int IdAnnonceTag { get; set; }
+            [ForeignKey("Tags")]
+            public int IdTag { get; set; }
+            //public virtual ICollection<int> Tag { get; set; }
+            [ForeignKey("Annonces")]
+            public int IdAnnonce { get; set; }
+            //public virtual ICollection<int> IdAnnonce { get; set; }
+        }
+        public class GeographicalSector
+        {
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Key]
+            public int IdGeographicalSector { get; set; }
+            [ForeignKey("IdAnnonce")]
+            public int IdAnnonce { get; set; }
+            [Required]
+            public int FirstPlace { get; set; } // Le premier lieu est requis, les utres sont optionnels
+            public int SecondPlace { get; set; }
+            public int ThirdPlace { get; set; }
+        }
+
         public class BloggingContext : DbContext
         {
-            public DbSet<Blog> Blogs { get; set; }
-            public DbSet<Post> Posts { get; set; }
-            public DbSet<User> Users { get; set; }
+            public DbSet<Users> Users { get; set; }
+            public DbSet<Annonces> Annonces { get; set; }
+            public DbSet<Comments> Comments { get; set; }
+            public DbSet<Transactions> Transactions { get; set; }
+            public DbSet<Tags> Tags { get; set; }
+            public DbSet<AnnoncesTags> AnnoncesTags { get; set; }
+            public DbSet<GeographicalSector> GeographicalSectors { get; set; }
         }
     }
 }
